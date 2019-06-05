33e14400d534bc4c2229b86b9e74049d0ce906e2
gribnoff <quinvex88@gmail.com>
2019-06-06 00:37:17 +0300

    Создан класс Course
    Конструкторы: с ручным вводом препятствий и случайными
    Метод doIt начинает полосу, результат прохождения полосы участниками выводит на экран, а так же записывает в карту команды для последующего обращения.

---
+++ src/Lesson_1/Marafon/Course.java
@@ -0,0 +1,53 @@
+package Lesson_1.Marafon;
+
+public class Course {
+    String name;
+    Obstacle[] obstacles;
+
+    public Course(String name, Obstacle... obstacles) {
+        this.name = name;
+        this.obstacles = obstacles;
+    }
+
+    public Course(String name, int number) {
+        this.name = name;
+        obstacles = new Obstacle[number];
+        for (int i = 0; i < number; i++) {
+            switch (i % 3) {
+                case 0: {
+                    obstacles[i] = new Cross(50 + (int)(Math.random() * 500));
+                    break;
+                }
+                case 1: {
+                    obstacles[i] = new Wall(2 + (int)(Math.random() * 5));
+                    break;
+                }
+                case 2: {
+                    obstacles[i] = new Water(10 + (int)(Math.random() * 20));
+                    break;
+                }
+            }
+        }
+    }
+
+    public void doIt(Team team) {
+        System.out.println("\nСоревнования " + name + " начались!\n");
+        boolean[][] resultTeamTable = new boolean[team.members.length][obstacles.length];
+
+        for (int i = 0; i < team.members.length; i++) {
+
+            team.members[i].setOnDistance();
+
+            for (int j = 0; j < obstacles.length; j++) {
+                resultTeamTable[i][j] = false;
+                obstacles[j].doIt(team.members[i]);
+                if (!team.members[i].isOnDistance())
+                    break;
+                else
+                    resultTeamTable[i][j] = true;
+            }
+        }
+
+        team.resultTable.put(this, resultTeamTable);
+    }
+}
