7272fb691e437d19d0c15accb48b13995cee6bff
gribnoff <quinvex88@gmail.com>
2019-06-09 22:23:19 +0300

    Написан Enum DayOfWeek

---
+++ src/Lesson_2/DayOfWeek/DayOfWeek.java
@@ -0,0 +1,19 @@
+package Lesson_2.DayOfWeek;
+
+public enum DayOfWeek {
+    MONDAY("Понедельник", 40),
+    TUESDAY("Вторник", 32),
+    WEDNESDAY("Среда", 24),
+    THURSDAY("Четверг", 16),
+    FRIDAY("Пятница", 8),
+    SATURDAY("Суббота", 0),
+    SUNDAY("Воскресенье", 0);
+
+    String rus;
+    int restWorkingHours;
+
+    DayOfWeek(String rus, int restWorkingHours) {
+        this.rus = rus;
+        this.restWorkingHours = restWorkingHours;
+    }
+}
