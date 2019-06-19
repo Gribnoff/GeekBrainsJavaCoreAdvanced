df557cca255a218e4c9ac29db6b9abed333df9e9
gribnoff <quinvex88@gmail.com>
2019-06-19 21:15:11 +0300

    Сравнение времени работы с разным количеством потоков

--- src/lesson_5/multitheading/Main.java
+++ src/lesson_5/multitheading/Main.java
@@ -1,13 +1,12 @@
 package lesson_5.multitheading;
 
 public class Main {
-    static final int size = 10000000;
-//    static final int size = 3;
-    static final int numberOfThreads = 6;
+    private static final int size = 10000000;
+    private static final int numberOfThreads = 2;
 
 
     public static void main(String[] args) {
-//        getTimeForGeneralCount();
+        getTimeForGeneralCount();
         getTimeForMultithreadedCount();
     }
 
@@ -19,10 +18,7 @@ public class Main {
 
         long startTime = System.currentTimeMillis();
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-
+        new Calculation(arr).run();
         System.out.printf("Время для обычного пересчёта - %dмс\n\n", System.currentTimeMillis() - startTime);
     }
 
@@ -32,22 +28,63 @@ public class Main {
             arr[i] = 1f;
         }
 
-        float[] resultArr = new float[size];
-
         long startTime = System.currentTimeMillis();
 
+        //разбиваем на несколько массивов
         float[][] splitArr = new float[numberOfThreads][];
         for (int i = 0; i < numberOfThreads; i++) {
             int startPos = (int)(i * (size * 1f / numberOfThreads));
             int endPos;
-//            if (i != numberOfThreads)
-                endPos = (int)((i + 1) * (size * 1f / numberOfThreads) - 1);
-//            else
-//                endPos = size - 1;
+            endPos = (int)((i + 1) * (size * 1f / numberOfThreads) - 1);
             splitArr[i] = new float[endPos - startPos + 1];
             System.arraycopy(arr, startPos, splitArr[i], 0, endPos - startPos + 1);
-            System.out.println(splitArr[i].length);
         }
-        System.out.printf("Общее время пересчёта(количество потоков: %d) - %d\n", numberOfThreads, System.currentTimeMillis() - startTime);
+        long arraySplitTime = System.currentTimeMillis();
+
+        //пересчитываем в несколько потоков
+        Thread[] threads = new Thread[numberOfThreads];
+        for (int i = 0; i < numberOfThreads; i++) {
+            threads[i] = new Thread(new Calculation(splitArr[i]));
+            threads[i].start();
+        }
+        for (int i = 0; i < numberOfThreads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        long arrayCalcTime = System.currentTimeMillis();
+
+        //склеиваем в один массив
+        int startPos = 0;
+        for (int i = 0; i < numberOfThreads; i++) {
+            System.arraycopy(splitArr[i], 0, arr, startPos, splitArr[i].length);
+            startPos += splitArr[i].length;
+        }
+        long arrayCollectTime = System.currentTimeMillis();
+
+
+        System.out.printf("Общее время пересчёта(количество потоков: %d) - %dмс\n", numberOfThreads, System.currentTimeMillis() - startTime);
+        System.out.println("из которых ушло на:");
+        System.out.printf("разбивку на несколько массивов - %dмс\n", arraySplitTime - startTime);
+        System.out.printf("пересчёт - %dмс\n", arrayCalcTime - arraySplitTime);
+        System.out.printf("ссклейку в один массив - %dмс\n", arrayCollectTime - arrayCalcTime);
+    }
+}
+
+class Calculation implements Runnable {
+    private float[] arr;
+
+    Calculation(float[] array) {
+        arr = array;
+    }
+
+    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
+    @Override
+    public void run() {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
     }
 }
